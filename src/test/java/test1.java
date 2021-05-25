9d07fb9673e67bdf7862f3b226aca7ffc02a6a8c
小苏 <1752549328@qq.com>
2021-05-25 11:58:57 +0800

    B组第一次提交

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -7,5 +7,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("A组第一次提交代码");
         System.out.println("A组第二次提交代码");
+        System.out.println("B组第一次提交代码");
     }
 }
