ca4c56bac4f55f88669bd508834533e83fe2f045
小苏 <1752549328@qq.com>
2021-05-25 10:22:39 +0800

    test1.java第一次提交

---
+++ src/test/java/test1.java
@@ -0,0 +1,10 @@
+/**
+ * @Author xiaosu
+ * @Date 2021/5/25  10:15
+ * TODO
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("A组第一次提交代码");
+    }
+}
